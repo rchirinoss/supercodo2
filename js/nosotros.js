9551b6b0d812dfca00f0b7c86f925f4deea0f981
RomySchab <rominaschab@hotmail.com>
2023-10-17 15:05:08 -0300

    modifique quienes somos, productos y json

--- js/nosotros.js
+++ js/nosotros.js
@@ -2,62 +2,65 @@ cad=`
 <h2>SUPER CODO presenta a sus creadores:</h2>
 <h1>@equipoCodo</h1>
 <div class="wrap">
-<div class="tarjeta-wrap">
-    <div class="tarjeta">
-        <div class="adelante card1"></div>
-        <div class="atras">
-            <h2>NOMBRE SIN PALABRA NOMBRE</h2><br><br>
-            <h3>EDAD: </h3><br>
-            <h3>CIUDAD: </h3><br>
-            <h3>OCUPACION:</h3><br>
-            <h3>EDUCACION:</h3><br>
-            <h3>HABILIDADES TECNICAS: </h3><br>
+    <div class="tarjeta-wrap">
+        <div class="tarjeta">
+            <div class="adelante card1">
+                <h2>NOMBRE</h2><br>
+            </div>
+            <div class="atras">
+                <h3>EDAD: </h3><br>
+                <h3>CIUDAD: </h3><br>
+                <h3>OCUPACION:</h3><br>
+                <h3>EDUCACION:</h3><br>
+                <h3>HABILIDADES TECNICAS: </h3><br>
+            </div>
         </div>
     </div>
-</div>
-<div class="tarjeta-wrap">
-    <div class="tarjeta">
-        <div class="adelante card2"></div>
-        <div class="atras">
-            <h2>NOMBRE SIN PALABRA NOMBRE</h2><br><br>
-            <h3>EDAD: </h3><br>
-            <h3>CIUDAD: </h3><br>
-            <h3>OCUPACION:</h3><br>
-            <h3>EDUCACION:</h3><br>
-            <h3>HABILIDADES TECNICAS: </h3><br>
+    <div class="tarjeta-wrap">
+        <div class="tarjeta">
+            <div class="adelante card2">
+                <h2>NOMBRE</h2><br>
+            </div>
+            <div class="atras">
+                <h3>EDAD: </h3><br>
+                <h3>CIUDAD: </h3><br>
+                <h3>OCUPACION:</h3><br>
+                <h3>EDUCACION:</h3><br>
+                <h3>HABILIDADES TECNICAS: </h3><br>
+            </div>
         </div>
     </div>
-</div>
-<div class="tarjeta-wrap">
-    <div class="tarjeta">
-        <div class="adelante card3"></div>
-        <div class="atras">
-            <h2>NOMBRE SIN PALABRA NOMBRE</h2><br><br>
-            <h3>EDAD: </h3><br>
-            <h3>CIUDAD: </h3><br>
-            <h3>OCUPACION:</h3><br>
-            <h3>EDUCACION:</h3><br>
-            <h3>HABILIDADES TECNICAS: </h3><br>
+    <div class="tarjeta-wrap">
+        <div class="tarjeta">
+            <div class="adelante card3">
+                <h2>NOMBRE</h2><br>
+            </div>
+            <div class="atras">
+                <h3>EDAD: </h3><br>
+                <h3>CIUDAD: </h3><br>
+                <h3>OCUPACION:</h3><br>
+                <h3>EDUCACION:</h3><br>
+                <h3>HABILIDADES TECNICAS: </h3><br>
+            </div>
         </div>
     </div>
-</div>
-</div>
-<div class="tarjeta-wrap">
-    <div class="tarjeta">
-        <div class="adelante card4"></div>
-        <div class="atras">
-            <h2>ROMINA SCHAB</h2><br><br>
-            <h3>EDAD: 41 años</h3><br>
-            <h3>CIUDAD: Trenque Lauquen</h3><br>
-            <h3>OCUPACION: Profesora de Ingles</h3><br>
-            <h3>EDUCACION: Tecnica Superior en Programacion</h3>
-            <h3>(UTN 2005)</h3><br>
-            <h3>HABILIDADES TECNICAS:</h3>
-            <h3>Idioma: Ingles</h3>
-            <h3>Tecnologias: HTML CSS JS GITHUB VUE</h3><br>
+    <div class="tarjeta-wrap">
+        <div class="tarjeta">
+            <div class="adelante card4">
+                <h2>ROMINA SCHAB</h2><br>
+            </div>
+            <div class="atras">
+                <h3>EDAD: 41 años</h3><br>
+                <h3>CIUDAD: Trenque Lauquen</h3><br>
+                <h3>OCUPACION: Profesora de Ingles</h3><br>
+                <h3>EDUCACION: Tecnica Superior en Programacion</h3>
+                <h3>(UTN 2005)</h3><br>
+                <h3>HABILIDADES TECNICAS:</h3>
+                <h3>Idioma: Ingles</h3>
+                <h3>Tecnologias: HTML CSS JS GITHUB VUE</h3><br>
+            </div>
         </div>
     </div>
-</div>
 </div>
 `
 document.querySelector("mainNosotros").innerHTML = cad 
