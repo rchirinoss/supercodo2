378f1464b6988d5b6719dd40c4d5b127199ba517
Estefania38 <leibaestefania@gmail.com>
2023-10-17 23:53:49 -0300

    sucursales

--- js/sucursales.js
+++ js/sucursales.js
@@ -1,12 +1,54 @@
-cad=`
-<div id="sucursales">
-    <div class="suc1">
+// cad=`
+// <div id="sucursales">
+//     <div class="suc1">
 
-    </div>
-    <div class="suc2">
+//     </div>
+//     <div class="suc2">
 
-    </div>
-</div>
-`
+//     </div>
+// </div>
+// `
 
-document.querySelector("main").innerHTML = cad
+// document.querySelector("main").innerHTML = cad
+
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+            url: './js/productos.json',
+            error: false,
+            sucursales: [],
+            datosFinales: [],
+            datos: [],
+            tipos: [],
+            nombre: ""
+        }
+    },
+    methods: {
+        fetchdata(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.sucursales = data.sucursales
+                    this.datos = data.sucursales
+                    this.datosFinales = data.sucursales
+                    for (elemento of this.datos) {
+                        if (this.tipos.indexOf(elemento.nombre) < 0)  //encuentra
+                            this.tipos.push(elemento.nombre)
+                    }
+                    console.log(this.tipos)
+                }
+                );
+        },
+     },
+
+    created() {
+        this.fetchdata(this.url)
+
+    },
+
+}).mount('#app')
+
+
+
